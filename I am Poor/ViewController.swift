ebe64a1462e93dfdb247761baac35ea592541af6
Heny <hyoh0606@gmail.com>
2021-06-10 17:22:23 +0900

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by 오현호 on 2021/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
